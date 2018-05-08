d6b0efabc119b93975b40c2566fd01ad2456587a
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-08 15:05:59 -0400

    pg-promise

--- server.js
+++ server.js
@@ -8,12 +8,18 @@ const PORT = process.env.PORT || 3001;
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    }
+
 
 app.get('/hey', (req, res) => {
-  console.log('hey');
-  res.json({'msg': 'hey there'})
-})
+      console.log('hey');
+      res.json({'msg': 'hey there'})
+    })
+
 
 
 
