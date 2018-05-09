fc12ecf7f4e5737a1f4d5413c3791c532d302766
student_23 <student_23@ool-ad0200e6.dyn.optonline.net>
2018-05-09 15:41:59 -0400

    added schema with image urls and map coordinates

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS favorites CASCADE;
 -- add create tables here
 CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), hashpassword VARCHAR(255);
 CREATE TABLE favorites(id SERIAL PRIMARY KEY, fave_notes VARCHAR(255), user_id INT REFERENCES users(id), vacations_id INT REFERENCES vacations(id) );
-CREATE TABLE vacations(id SERIAL PRIMARY KEY, location VARCHAR(255), description TEXT, map_coordinates DECIMAL(18,15), img_url TEXT);
+CREATE TABLE vacations(id SERIAL PRIMARY KEY, location VARCHAR(255), description TEXT, latitude DECIMAL(18,15), longitude DECIMAL(18,15), img_url TEXT);
 
 -- create users table
 INSERT INTO users
@@ -33,29 +33,29 @@ VALUES
 -- create vacations table
 
 INSERT INTO vacations
- (location, description, map_coordinates, img_url)
+ (location, description, latitude, longitude, img_url)
 VALUES
-( 'Hawaii', 'Amazing and exotic island with thrilling volcanos and lush gardens.', './hawaii1.jpeg' ),
-( 'Florida', 'Warm and inviting.', './florida2.jpeg'),
-( 'New York', 'This is truly the city that never sleeps!','./newyork1.jpeg'),
-( 'California', 'Sun and fun every day!','./california1.jpeg'),
-( 'Austrailia', 'The is the land of the down under with tons of fun and beauty.','./austrailia-sydney.jpeg')
-( 'Mexico', 'Spicy food and music and life with plenty of sun.','./mexico1.jpeg')
-( 'Barbados', 'Crystal clear waters and amazing island cuisine.', './barbados1.jpeg')
-( 'Italy', 'Beautiful views and excellent entertainment.', './italy1.jpeg'),
-( 'Brazil', 'Music with laughter and fun for all.', './brazil.jpeg'),
-( 'Cayman Islands', 'The white sandy beaches are endless and peaceful.', './cayman-island1.jpeg'),
-( 'Paris', 'This is truly the land of love and romance.', './paris1.jpeg'),
-( 'British Columbia', 'This places feels so royal!', './british-columbia1.jpeg'),
-( 'Singapore', 'The is the place where exquisite design meets beauty and technology.', './singapore1.jpeg'),
-( 'Ecuador', 'Best place for delicious crabs!', './ecuador1.jpeg'),
-( 'Kenya', 'Exotic and rural at the same time!', './kenya1.jpeg'),
-( 'Ethiopia', 'Full of tribal history.', './ethiopia1.jpeg'),
-( 'India', 'Full of life and color.', './india1.jpeg',),
-( 'China', 'Land of wisdom and poise.', './china1.jpeg'),
-( 'Germany', 'Much history and many places of beauty.', './germany1.jpeg'),
-( 'Japan', 'This place is rich with culture and music. They are very skilled in the arts!', './japan1.jpeg'),
-( 'Alaska', 'So cold yet so lovely!', './alaska1.jpeg'),
+( 'Hawaii', 'Amazing and exotic island with thrilling volcanos and lush gardens.', 19.8967662,-155.58278180000002, './hawaii1.jpeg' ),
+( 'Florida', 'Warm and inviting.',27.6648274,-81.51575350000002, './florida2.jpeg'),
+( 'New York', 'This is truly the city that never sleeps!',40.7127753,-74.0059728,'./newyork1.jpeg'),
+( 'California', 'Sun and fun every day!',36.778261,-119.41793239999998,'./california1.jpeg'),
+( 'Austrailia', 'The is the land of the down under with tons of fun and beauty.',-25.274398,133.77513599999997,'./austrailia-sydney.jpeg')
+( 'Mexico', 'Spicy food and music and life with plenty of sun.',23.634501,-102.55278399999997,'./mexico1.jpeg')
+( 'Barbados', 'Crystal clear waters and amazing island cuisine.',13.193887,-59.54319799999996, './barbados1.jpeg')
+( 'Italy', 'Beautiful views and excellent entertainment.',41.87194,12.567379999999957, './italy1.jpeg'),
+( 'Brazil', 'Music with laughter and fun for all.',-14.235004,-51.92527999999999, './brazil.jpeg'),
+( 'Cayman Islands', 'The white sandy beaches are endless and peaceful.',19.3133,-81.25459999999998, './cayman-island1.jpeg'),
+( 'Paris', 'This is truly the land of love and romance.',48.856614,2.3522219000000177, './paris1.jpeg'),
+( 'British Columbia', 'This places feels so royal!',53.7266683,-127.64762050000002, './british-columbia1.jpeg'),
+( 'Singapore', 'The is the place where exquisite design meets beauty and technology.',1.352083,103.81983600000001, './singapore1.jpeg'),
+( 'Ecuador', 'Best place for delicious crabs!',-1.831239,-78.18340599999999, './ecuador1.jpeg'),
+( 'Kenya', 'Exotic and rural at the same time!',-0.023559,37.90619300000003, './kenya1.jpeg'),
+( 'Ethiopia', 'Full of tribal history.',9.145000000000001,40.48967300000004, './ethiopia1.jpeg'),
+( 'India', 'Full of life and color.',20.593684,78.96288000000004, './india1.jpeg',),
+( 'China', 'Land of wisdom and poise.',35.86166,104.19539699999996, './china1.jpeg'),
+( 'Germany', 'Much history and many places of beauty.',51.165691,10.451526000000058, './germany1.jpeg'),
+( 'Japan', 'This place is rich with culture and music. They are very skilled in the arts!',36.204824,138.252924, './japan1.jpeg'),
+( 'Alaska', 'So cold yet so lovely!',64.2008413,-149.4936733, './alaska1.jpeg'),
 
 
 
